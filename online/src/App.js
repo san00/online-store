5960e09f9a158625288eb9097d88755fa4e81df8
sanscomp <sanscomp@anslems-iMac.lan>
2020-07-04 20:24:48 +0100

    Setup routing

--- online/src/App.js
+++ online/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+
 import './App.css';
+import Signin from './components/Signin';
+import Checkout from './components/Checkout';
+import Signup from './components/Signup';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-    <p>hello</p>
-    </div>
+    <Router>
+      <p>hello</p>
+      <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/checkout" component={Checkout} />
+    </Router>
   );
 }
 
