62b844fbd87ce3880e0b67c2fb7f3e0891744a2b
sanscomp <sanscomp@anslems-iMac.lan>
2020-07-23 20:36:49 +0100

    Load brand images

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -3,9 +3,12 @@ import React from "react";
 function Brand({ brand }) {
   return (
     <article>
-      <p>{brand.name ? brand.name : "Unavailable"}</p>
-      <p>{brand.description ? brand.description : "Unavailable"}</p>
-      <img src={`http:${brand.image.url}`} alt="brand logo"></img>
+       <img
+        src={`http://localhost:1337${brand.image[0].url}`}
+        alt="brand logo"
+      ></img> 
+      <p>{brand.name ? brand.name : "Name unavailable"}</p>
+      <p>{brand.description ? brand.description : "Description unavailable"}</p> 
     </article>
   );
 }
