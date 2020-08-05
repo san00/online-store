000cc19ffd8e0239318e873290c349eca4d36612
sanscomp <sanscomp@anslems-iMac.lan>
2020-08-05 21:36:55 +0100

    Add items to cart - Set context value onClick

--- src/components/Brew.js
+++ src/components/Brew.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "./CartContext";
 import styled from "styled-components";
 
 function Brew({ brew }) {
+  const context = useContext(CartContext);
+
   const Image = styled.img`
     width: 15em;
   `;
@@ -15,7 +18,29 @@ function Brew({ brew }) {
       <p>{brew.name ? brew.name : "Unavailable"}</p>
       <p>{brew.description ? brew.description : "Unavailable"}</p>
       <p>£{brew.price ? brew.price : "Unavailable"}</p>
-      <button type="submit"> Add to cart </button>
+
+      {/* Update CartContext value */}
+      <button
+        type="submit"
+        onClick={() => {
+          let quantity = 1;
+          //check whether brew exists
+          if (context.cart[brew.id]) {
+            quantity = context.cart[brew.id].quantity + 1;
+          }
+          context.setCart({
+            ...context.cart,
+            [brew.id]: {
+              price: brew.price,
+              name: brew.name,
+              quantity: quantity,
+            },
+          });
+        }}
+      >
+        Add to cart
+      </button>
+      {context.cart[brew.id] && <p>"item added to cart"</p>}
     </div>
   );
 }
