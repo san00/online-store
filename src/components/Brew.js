e13e9f50b9ee8f24ea8186033770b38385046b36
sanscomp <sanscomp@anslems-iMac.lan>
2020-08-18 22:00:19 +0100

    Display brand & related products

--- src/components/Brew.js
+++ src/components/Brew.js
@@ -1,46 +1,20 @@
-import React, { useContext } from "react";
-import { CartContext } from "./CartContext";
+import React from "react";
 import styled from "styled-components";
 
 function Brew({ brew }) {
-  const context = useContext(CartContext);
-
   const Image = styled.img`
     width: 15em;
   `;
 
   return (
     <div>
+      <p>{brew.name}</p>
       <Image
         src={`http://localhost:1337${brew.image.url}`}
         alt="brew logo"
       ></Image>
-      <p>{brew.name ? brew.name : "Unavailable"}</p>
-      <p>{brew.description ? brew.description : "Unavailable"}</p>
-      <p>£{brew.price ? brew.price : "Unavailable"}</p>
-
-      {/* Update CartContext value */}
-      <button
-        type="submit"
-        onClick={() => {
-          let quantity = 1;
-          //check whether brew exists
-          if (context.cart[brew.id]) {
-            quantity = context.cart[brew.id].quantity + 1;
-          }
-          context.setCart({
-            ...context.cart,
-            [brew.id]: {
-              price: brew.price,
-              name: brew.name,
-              quantity: quantity,
-            },
-          });
-        }}
-      >
-        Add to cart
-      </button>
-      {context.cart[brew.id] && <p>"item added to cart"</p>}
+      <p>{brew.description}</p>
+      <p>{brew.price}</p>
     </div>
   );
 }
