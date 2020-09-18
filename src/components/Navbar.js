c45219b7f5adcf55bd073b0321a0489a84f6c98c
sanscomp <sanscomp@anslems-iMac.lan>
2020-09-18 19:12:47 +0100

    Add cart logo to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,22 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Cart from "./Cart";
 
-import Flex from "../styles/Flex";
-import Container from "../styles/Container";
 import Heading from "../styles/Heading";
+import Nav, {NavAlignRight, NavAlignLeft } from "../styles/Navbar";
 
 function Navbar() {
   return (
-    <Container>
-      <Flex justifyAround>
-        <Heading>Shop-A-Lot</Heading>
-      </Flex>
-      <Flex justifyAround>
-        <Link to="/signup">Login</Link>
-        <Cart />
-        <Link to="/signin">Signup</Link>
-      </Flex>
-    </Container>
+    <Nav>
+      <Heading>Shop-A-Lot</Heading>
+      <NavAlignRight><Cart/></NavAlignRight>
+      <NavAlignLeft ><Link to="/signin">Signup</Link></NavAlignLeft>
+      <Link to="/signup">Login</Link>
+    </Nav>
+ 
   );
 }
 
